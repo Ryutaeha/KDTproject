ea4772e2df6a593b853d135258a8349fdcfb9035
Ryutaeha <taeha61@gmail.com>
2023-11-08 22:28:24 +0900

    초기 커밋.

---
+++ KDTproject/Date.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    internal class Date
+    {
+        public static string[] startLogo =
+        {
+            " @@@@            @          @    @         @@@@@@",
+            "@@  @           @@          @    @          @   @",
+            "@   @           @@          @@  @@          @   @",
+            "@   @          @  @         @@  @@          @ @ ",
+            "@              @  @         @@  @@          @ @ ",
+            "@  @@@         @  @         @ @@ @          @@@  ",
+            "@   @         @@@@@         @ @  @          @ @  ",
+            "@   @         @@  @         @ @  @          @   @ ",
+            "@   @         @   @         @    @          @   @ ",
+            " @@@@        @@@ @@@        @@   @@        @@@@@@ "
+
+        };
+
+    public static string[] startMenu = { "새 게임", "이어하기", "종료" };
+    }
+    internal class keyEvent
+    {
+        /// <summary>
+        /// 키 이벤트, 현재 선택되어 있는 메뉴, 메뉴 총 길이
+        /// </summary>
+        /// <param name="keyInfo"></param>
+        /// <param name="selectMenu"></param>
+        /// <param name="menuLength"></param>
+        /// <returns></returns>
+        public static (bool,int) keyCheck(ConsoleKeyInfo keyInfo, int selectMenu, int menuLength)
+        {
+        keyInfo = Console.ReadKey();
+
+        if (keyInfo.Key == ConsoleKey.UpArrow)
+        {
+            if (selectMenu > 0)
+            {
+                selectMenu--;
+            }
+        }
+
+            // ↓ 키 처리
+        if (keyInfo.Key == ConsoleKey.DownArrow)
+        {
+            if (selectMenu < menuLength - 1)
+            {
+               selectMenu++;
+            }
+        }
+        if(keyInfo.Key == ConsoleKey.Enter)
+        {
+            return (true, selectMenu);
+        }
+        return (false,selectMenu);
+        }
+    }
+    
+    internal class Draw
+    {
+    public static void WriteText(string text, int xOffset, int yOffset)
+    {
+        Console.SetCursorPosition(xOffset, yOffset);
+        Console.Write(text);
+    }
+
+
+    /// <summary>
+    /// int 배열[4] 너비, 높이 , 시작 X축, 시작 Y축
+    /// </summary>
+    public static void DrawBox(int[] sceneArea)
+    {
+        // 상하 벽 그리기
+        for (int i = 0; i < sceneArea[0] - 1; i++)
+        {
+            // i+시작 X, Y
+            Console.SetCursorPosition(i + sceneArea[2], sceneArea[3]);
+            Console.Write("-");
+            // i+시작 X, Y시작점 + 높이
+            Console.SetCursorPosition(i + sceneArea[2], sceneArea[3] + sceneArea[1]);
+            Console.Write("-");
+        }
+        // 좌우 벽 그리기
+        for (int i = 0; i < sceneArea[1] - 1; i++)
+        {
+            // 시작 X - 1 , i + 시작 Y축 +1
+            Console.SetCursorPosition(sceneArea[2] - 1, i + sceneArea[3] + 1);
+            Console.Write("|");
+            // (시작 X -1) + 너비 , i +  시작 Y + 1
+            Console.SetCursorPosition(sceneArea[2] - 1 + sceneArea[0], i + sceneArea[3] + 1);
+            Console.Write("|");
+
+        }
+        Console.WriteLine();
+    }
+    public static void ClearBox(int[] sceneArea)
+    {
+        for (int i = 1; i < sceneArea[1]; i++)
+        {
+            string clearText = "";
+            for(int j = 1; j < sceneArea[0]; j++)
+            {
+                clearText += " ";
+            }
+            WriteText(clearText, sceneArea[2], sceneArea[3]+i);
+        }
+    }
+
+        /*
+    static void DrawBox(int[] widthArea, int[] heightArea)
+    {
+        // 상하 벽 그리기
+        for (int i = 0; i < widthArea[0]-1; i++)
+        {
+                                      //시작 X, Y
+            Console.SetCursorPosition(i + widthArea[1], widthArea[2]);
+            Console.Write("-");
+                                      //시작 X, Y시작점 + 높이
+            Console.SetCursorPosition(i + widthArea[1], widthArea[2] + heightArea[0]);
+            Console.Write("-");
+        }
+
+        // 좌우 벽 그리기
+        for (int i = 0; i < heightArea[0] - 1; i++)
+        {
+            Console.SetCursorPosition(heightArea[1], i + heightArea[2]);
+            Console.Write("|");
+            Console.SetCursorPosition(heightArea[1] + widthArea[0], i + heightArea[2]);
+            Console.Write("|");
+
+        }
+        Console.WriteLine();
+    }
+     */
+}
+
---
+++ KDTproject/Program.cs
@@ -0,0 +1,117 @@
+﻿
+
+using static System.Net.Mime.MediaTypeNames;
+
+namespace KDTproject
+{
+
+    internal class Program
+    {
+        // 화면 벽그리기 순서대로 넓이, 높이, 시작 X좌표, 시작 Y좌표
+        static int[] windowWidth = { 90, 25, 1, 0 };
+        static int[] info = { 50, 12, 20, 6 };
+        static ConsoleKeyInfo keyInfo;
+
+
+
+        // 메인 함수
+        static void Main(string[] args)
+        {    
+            Console.Title = "KDT PROJECT HOME";
+            bool gameStart = StartScene();
+            if (!gameStart)
+            {
+                return;
+            }
+        }
+
+
+        // 첫 스타트 씬
+        static bool StartScene()
+        {
+            int selectMenu = 0;
+            bool firstLord = true;
+            bool sceneEnd = false;
+            string[] startLogo = Date.startLogo;
+            string[] startMenu = Date.startMenu;
+
+            while(!sceneEnd) 
+            {
+                Console.Clear();
+
+                // 화면 벽그리기 순서대로 넓이, 높이, 시작 X좌표, 시작 Y좌표
+                Draw.DrawBox(windowWidth);
+             
+                for(int i = 0; i< startLogo.Length; i++)
+                {
+                    Draw.WriteText(startLogo[i], 20, 2+i);
+                    if (firstLord)
+                    {
+                        Thread.Sleep(200);
+                    }
+                }
+                firstLord = false;
+                Draw.WriteText("by 태하 With Sparta", 37, 14);
+
+                int[] selectBtn = {30 ,4, 31, 18 };
+                Draw.DrawBox(selectBtn);
+                for(int i = 0; i<startMenu.Length; i++)
+                {
+                    if(i == selectMenu)
+                    {
+                        Draw.WriteText(">", 40, 19 + i);
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.BackgroundColor = ConsoleColor.White;
+                    }
+                    Draw.WriteText(startMenu[i], 42, 19+i);
+                    Console.ResetColor();
+                }
+                
+
+                (bool sceneCheck, int selectMenuCheck) = keyEvent.keyCheck(keyInfo,selectMenu,startMenu.Length);
+                sceneEnd = sceneCheck;
+                selectMenu = selectMenuCheck;
+
+                if(selectMenu == 0 && sceneEnd)
+                {    
+                    Draw.DrawBox(info);
+                    Draw.ClearBox(info);
+                    Draw.WriteText("구현중...", 40, 12);
+                    sceneEnd = false;
+                    
+                    Thread.Sleep(1000);
+                    
+                }
+                if(selectMenu == 1 && sceneEnd)
+                {
+                    Draw.DrawBox(info);
+                    Draw.ClearBox(info);
+                    Draw.WriteText("구현중...", 40, 12);
+                    sceneEnd = false;
+
+                    Thread.Sleep(1000);
+                }
+                if (selectMenu == 2 && sceneEnd)
+                {
+                    Console.SetCursorPosition(0,26);
+                    sceneEnd = true;
+                }
+            }
+            if(selectMenu == 2)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        // 
+        static void MainScene()
+        {
+            //이어하기시 읽은 데이터를 바탕으로 캐릭터를 생성
+            //새로하기시 데이터 새로 생성
+            //if()
+        }
+        
+
+    }
+}
